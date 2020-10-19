7215074ffb78b62208d485c8904c4655b5d43f70
gbalduzz <gbalduzz@itp.phys.ethz.ch>
2020-10-19 19:52:13 +0200

    fixup

--- test/performance/lazy_evaluation_perftest.cpp
+++ test/performance/lazy_evaluation_perftest.cpp
@@ -17,33 +17,36 @@ static void BM_ContiguousLazyEvaluation(benchmark::State& state) {
 }
 BENCHMARK(BM_ContiguousLazyEvaluation);
 
-
 static void BM_ContiguousBaselineEvaluation(benchmark::State& state) {
-  nd::seed(0);
-  auto A = rand<float>(n, n, n);
-  auto B = rand<float>(n, n, n);
-  auto C = rand<float>(n, n, n);
+  std::mt19937_64 rng(0);
+  std::uniform_real_distribution<float> distro(0, 1);
 
-  for (auto _ : state) {
-    for(std::size_t i = 0; i < A.size(); ++i)
+  std::vector<float> A(n * n * n), B(n * n * n), C(n * n * n);
+  for (std::size_t i = 0; i < A.size(); ++i) {
+    A[i] = distro(rng);
+    B[i] = distro(rng);
+    C[i] = distro(rng);
+  }
+
+  for (auto&& _ : state) {
+    for (std::size_t i = 0; i < C.size(); ++i)
       C[i] = C[i] - A[i] / (2. * B[i]);
   }
 }
-BENCHMARK(BM_ContiguousBaselineEvaluation);
 
-constexpr std::size_t n2 = 200;
+BENCHMARK(BM_ContiguousBaselineEvaluation);
 static void BM_NonContiguousLazyEvaluation(benchmark::State& state) {
   nd::seed(0);
-  auto A = rand<float>(n2, n2, n2);
-  auto B = rand<float>(n2, n2, n2);
-  auto C = rand<float>(n2, n2, n2);
+  auto A = rand<float>(n, n, 5ul, n, 4ul);
+  auto B = rand<float>(n, n, 5ul, n, 4ul);
+  auto C = rand<float>(n, n, 5ul, n, 4ul);
+  auto res = NDArray<float, 3>(n, n, n);
 
   for (auto _ : state) {
-    C(all, 0, all) = C(all, 0, all) - A(all, 0, all) / (2. * B(all, 0, all));
+    res = C(all, all, 0, all, 0) - A(all, all, 0, all, 0) / (2. * B(all, all, 0, all, 0));
   }
 }
 BENCHMARK(BM_NonContiguousLazyEvaluation);
-
 
 static void BM_NonContiguousBaselineEvaluation(benchmark::State& state) {
   nd::seed(0);
@@ -52,8 +55,8 @@ static void BM_NonContiguousBaselineEvaluation(benchmark::State& state) {
   auto C = rand<float>(n2, n2, n2);
 
   for (auto _ : state) {
-    for(std::size_t i = 0; i < n2; ++i)
-      for(std::size_t j = 0; j < n2; ++j)
+    for (std::size_t i = 0; i < n2; ++i)
+      for (std::size_t j = 0; j < n2; ++j)
         C(i, 0, j) = C(i, 0, j) - A(i, 0, j) / (2. * B(i, 0, j));
   }
 }
