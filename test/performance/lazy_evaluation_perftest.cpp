4458f5f774134801ac6af51d805c0807db6d5d64
gbalduzz <gbalduzz@itp.phys.ethz.ch>
2020-10-19 20:03:34 +0200

    upadted tests to match xtensor

--- test/performance/lazy_evaluation_perftest.cpp
+++ test/performance/lazy_evaluation_perftest.cpp
@@ -47,17 +47,3 @@ static void BM_NonContiguousLazyEvaluation(benchmark::State& state) {
   }
 }
 BENCHMARK(BM_NonContiguousLazyEvaluation);
-
-static void BM_NonContiguousBaselineEvaluation(benchmark::State& state) {
-  nd::seed(0);
-  auto A = rand<float>(n2, n2, n2);
-  auto B = rand<float>(n2, n2, n2);
-  auto C = rand<float>(n2, n2, n2);
-
-  for (auto _ : state) {
-    for (std::size_t i = 0; i < n2; ++i)
-      for (std::size_t j = 0; j < n2; ++j)
-        C(i, 0, j) = C(i, 0, j) - A(i, 0, j) / (2. * B(i, 0, j));
-  }
-}
-BENCHMARK(BM_NonContiguousBaselineEvaluation);
